85704c6c45a9f0aa5f8902b82132b9eb1780e25e
naz_liton <nazrulliton@icloud.com>
2019-12-04 14:02:25 +0000

    backend db validation added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,21 +1,43 @@
 use hotel;
 db.dropDatabase();
 
+db.createCollection("bookings", {
+  validator:{
+    $jsonSchema: {
+      bsonType: "object",
+      required: ['name', 'email', 'checkInStatus'],
+      properties: {
+        name: {
+          bsonType: "string",
+          description: "needs to be a string"
+        },
+        email:{
+          bsonType: "string",
+          description: "needs to be a string"
+        },
+        checkInStatus:{
+          bsonType: "string",
+          description: "needs to be a string"
+        }
+      }
+    }
+  }
+})
 
 db.bookings.insertMany([
   {
     name: "Samuel Jackson",
     email: "Samjackson@gmail.com",
-    checkInStatus: true,
+    checkInStatus: "true",
   },
   {
     name: "Marc Davidson",
     email: "mdavidson@gmail.com",
-    checkInStatus: false,
+    checkInStatus: "false",
   },
   {
     name: "Leo Messi",
     email: "Lmessi@gmail.com",
-    checkInStatus: true,
+    checkInStatus: "true",
   }
 ]);
