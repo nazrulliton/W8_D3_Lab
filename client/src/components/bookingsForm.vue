b221b117c6d9af5221c4113cea1fe3a72800556b
naz_liton <nazrulliton@icloud.com>
2019-12-04 12:17:53 +0000

    able to add to db

--- client/src/components/bookingsForm.vue
+++ client/src/components/bookingsForm.vue
@@ -1,9 +1,48 @@
 <template lang="html">
+<form id="bookingsForm" v-on:submit.prevent="handleSubmit">
+
+<label for="name">Name</label>
+<input type="text" name="" value="" v-model="name">
+
+<label for="email">Email address</label>
+<input type="text" name="" value="" v-model="email">
+
+<label for="CheckedIn">Checked In</label>
+<select  name="" v-model="checkInStatus">
+  <option value="True">Yes</option>
+  <option value="False">No</option>
+</select>
+<input type="submit" name="" value="Submit Booking">
+
+
+</form>
+
+
 
 </template>
 
 <script>
+import BookingService from '@/services/BookingService.js'
+
 export default {
+  name: "bookings-form",
+  data(){
+    return {
+      name:"",
+      email: "",
+      checkInStatus: ""
+    }
+  },
+  methods: {
+    handleSubmit(){
+      const booking = {
+        name: this.name,
+        email: this.email,
+        checkInStatus: this.checkInStatus
+      }
+      BookingService.postBooking(booking);
+    }
+  }
 }
 </script>
 
